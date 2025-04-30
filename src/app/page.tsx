f60e0f3d3f80eecdd3bbb5610be7287ce0c4d523
mahesh ch <chmahesh.dev@gmail.com>
2025-05-01 00:26:10 +0530

    included temp doc btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/About";
 import Education from "@/components/Education";
+import Notes from "@/components/notes/Notes";
 import Projects from "@/components/Projects";
 import TechStackComponent from "@/components/TechStack";
 
 export default function Home() {
   return (
     <div>
+      <Notes />
       <About />
       <TechStackComponent />
       <Projects />
---
+++ src/components/notes/Notes.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Notes() {
+  return (
+    <div className="flex items-center">
+      <div>
+        <h1 className=" font-medium text-2xl">
+          ➡️{" "}
+          <span className=" text-green-600">
+            {" "}
+            <Link
+              href={
+                "https://massive-dodo-f8e.notion.site/MANOJ-SIR-CLASS-1e576887e01a80bbb228f33850ae2b74"
+              }
+            >
+              {" "}
+              CLICK HERE TO OPEN DOC !
+            </Link>
+          </span>
+        </h1>
+      </div>
+    </div>
+  );
+}
