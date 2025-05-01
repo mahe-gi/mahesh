b9a3aae262136c3bc17d7fdd753a28b8a95543b4
Mahesh Ch <chmahesh.dev@gmail.com>
2025-05-01 15:38:36 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import About from "@/components/About";
 import Education from "@/components/Education";
-import Notes from "@/components/notes/Notes";
 import Projects from "@/components/Projects";
 import TechStackComponent from "@/components/TechStack";
 
 export default function Home() {
   return (
     <div>
-      <Notes />
       <About />
       <TechStackComponent />
       <Projects />
